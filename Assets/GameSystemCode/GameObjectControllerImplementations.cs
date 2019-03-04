e6e11d425fa5996c8ca0de087067c9b639aa11ce
Alex Kennedy <KennedyAlexJohn@gmail.com>
2019-03-04 22:35:26 +1100

    wrote some scaffolding for the game object controllers. Those are the biggest TODOs and will allow us to actually  creat in-game elements!

---
+++ Assets/GameSystemCode/GameObjectControllerImplementations.cs
@@ -0,0 +1,19 @@
+﻿using BeatBlockSystem;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameObjectControllerImplementations {
+    public class AnimationGameObjectController : IAnimationGameObjectController {
+        // TODO - DESIGN AND IMPLEMENT THIS!
+    }
+
+    public class HitboxGameObjectController : IHitboxGameObjectController {
+        // TODO - DESIGN AND IMPLEMENT THIS!
+
+        // should this be a monobehaviour? Or wrap a monobehaviour?
+    }
+}
