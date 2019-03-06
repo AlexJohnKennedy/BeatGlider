e240f022922799a7196322579738cf7a9e32c4a6
Alex Kennedy <KennedyAlexJohn@gmail.com>
2019-03-06 18:37:35 +1100

    added some basic Animation curve implementations, will be enough for now!

---
+++ Assets/GameSystemCode/AnimationCurveImplementations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeatBlockSystem;
+
+namespace BeatBlockSystem.AnimationCurveImplementations {
+    public class DefaultLinearCurve : IAnimationCurve {
+        // All this does is return the same time; it doesn't adjust the curve at all!
+        public float MapTimeToAnimationPercentage(float time) {
+            return time;
+        }
+    }
+    public class DefaultQuadraticCurve : IAnimationCurve {
+        public float MapTimeToAnimationPercentage(float time) {
+            return time * time;
+        }
+    }
+}
--- Assets/GameSystemCode/GameObjectControllerImplementations.cs
+++ Assets/GameSystemCode/GameObjectControllerImplementations.cs
@@ -9,11 +9,33 @@ using System.Threading.Tasks;
 namespace GameObjectControllerImplementations {
     public class AnimationGameObjectController : IAnimationGameObjectController {
         // TODO - DESIGN AND IMPLEMENT THIS!
+        public int AnimationTypeId => throw new NotImplementedException();
+
+        public bool StartAnimation(GridPosition offset, float scalingFactor, float speed, int comboFactor) {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(float timeIndex) {
+            throw new NotImplementedException();
+        }
     }
 
     public class HitboxGameObjectController : IHitboxGameObjectController {
         // TODO - DESIGN AND IMPLEMENT THIS!
 
         // should this be a monobehaviour? Or wrap a monobehaviour?
+        public int HitboxTypeId => throw new NotImplementedException();
+
+        public float HitDelayOffset => throw new NotImplementedException();
+
+        public float HitboxDuration => throw new NotImplementedException();
+
+        public bool StartAnimation(GridPosition offset, float sizeScalingFactor, float playbackSpeedScalingFactor) {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(float timeIndex) {
+            throw new NotImplementedException();
+        }
     }
 }
